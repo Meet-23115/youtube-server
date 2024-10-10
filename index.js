d32ca505635d73a68ecc0f7088c40b82543e02fa
Meet Gelothra <imailmeet4000@gmail.com>
2024-10-10 21:23:47 +0530

    no errors and added proffesional init

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 
-const client = require('./db/mongo');
+const client = require('./src/db/mongo');
 
 async function run() {
     try {
